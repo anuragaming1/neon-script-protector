095b579006ca1363bb510d58262f2e25b401e968
Youtube: Anura Gaming <nhat75141@gmail.com>
2025-11-23 21:17:16 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: false,
-  },
-  trailingSlash: false,
+  // Bỏ experimental config
 }
 
 module.exports = nextConfig
